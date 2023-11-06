9df5d2b855334af6eca9559ac306e396042d0d37
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-06 07:32:51 -0300

    added code to PessoaDao

---
+++ IMD/UFRN/BR/dao/PessoaDAO.java
@@ -0,0 +1,11 @@
+package IMD.UFRN.BR.dao;
+
+public class PessoaDAO {
+	
+	public void cadastrarPessoa(Pessoa);
+	public void removerPessoa(Pessoa);
+	public void listarPessoas();
+	public void calcularTributosPessoas();
+	public void imprimeImpostoTotal();
+	
+}
