00e682ae1dd78fa4b1337d6069e25b6cd414c099
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-11-06 07:25:04 -0300

    added ContaCorrente code

---
+++ IMD/UFRN/BR/modelo/ContaCorrente.java
@@ -0,0 +1,22 @@
+package IMD.UFRN.BR.modelo;
+
+public class ContaCorrente {
+	
+	private String agencia;
+	private	String numero;
+	private double saldo;
+	
+	
+	public void sacar(double valor) {
+		
+	}
+	
+	public void depositar(double valor) {
+		
+	}
+	
+	public boolean transferir(double valor, ContaCorrente cc) {
+		
+	}
+	
+}
